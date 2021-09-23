adaf5d645708d73e285ee2a62650785dbf42f06f
angrykoala <angrykoala@outlook.es>
2021-09-23 23:54:52 +0100

    Hand scene improved

--- entities/player/hands/hand.gd
+++ entities/player/hands/hand.gd
@@ -11,6 +11,25 @@ export(HAND) var hand=HAND.RIGHT
 onready var caught_area: Area=$CaughtArea
 onready var fruits_container: Spatial=$Fruits
 
+onready var left_mesh=$LeftHandMesh
+onready var right_mesh=$RightHandMesh
+
+onready var thumb_collision_left=$ThumbCollisionLeft
+onready var thumb_collision_right=$ThumbCollisionRight
+
+func _ready():
+	match hand:
+		HAND.RIGHT:
+			left_mesh.visible=false
+			right_mesh.visible=true
+			thumb_collision_left.queue_free()
+		HAND.LEFT:
+			left_mesh.visible=true
+			right_mesh.visible=false
+			thumb_collision_right.queue_free()
+
+
+
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector3()
