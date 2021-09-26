10406d66d03b70bbf5a1eae8804c91ae698a2383
angrykoala <angrykoala@outlook.es>
2021-09-26 21:50:33 +0100

    adjust credits

--- scenes/results.gd
+++ scenes/results.gd
@@ -3,7 +3,8 @@ extends Spatial
 
 export(String, FILE) var main_menu_path
 
-onready var results=$CanvasLayer/Panel/Label
+onready var points=$CanvasLayer/Panel/Points
+onready var results=$CanvasLayer/Panel/ScrollContainer/FruitList
 
 func _on_ok_pressed() -> void:
 	Utils.click.play()
@@ -16,4 +17,4 @@ func _ready():
 		var count=Utils.frankenfruits_list[fruit][1]
 		# results.text=results.text+"\n"+str(count)+" x "+frankenfruit.nombre+" = "+str(count*frankenfruit.puntuation)
 		results.text=results.text+"\n"+str(count)+" x "+fruit
-	results.text=results.text+"\n\n Total: "+str(Utils.score)
+	points.text="Puntos: "+str(Utils.score)
