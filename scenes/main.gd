5c46a4485fbc3507b1b265fd44078639418f2978
angrykoala <angrykoala@outlook.es>
2021-09-26 15:35:19 +0100

    Time and results menu

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,21 @@
 extends Spatial
 
+export var time_left:int=100
+export(String, FILE) var results_scene_path
+
+onready var time_label:Label=$HUD/TimeLabel
+
 
 func _ready() -> void:
 	randomize()
+
+
+func _on_time_tick() -> void:
+	time_left-=1
+	time_label.text="Time: "+String(time_left)
+	if time_left<=0:
+		on_game_finish()
+
+
+func on_game_finish():
+	get_tree().change_scene(results_scene_path)
